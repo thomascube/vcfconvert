ac975b9e5be203c834334e96fb1663fbafc48cf6
thomascube <thomas@roundcube.net>
2007-12-02 18:41:05 +0000

    Fix utf-8 detection

--- vcard_convert.php
+++ vcard_convert.php
@@ -78,7 +78,7 @@ class vcard_convert extends Contact_Vcard_Parse
 	 */
 	function fromText($text, $decode_qp = true)
 	{
-		if ($encoding = vcard_convert::get_encoding($text))
+		if ($encoding = vcard_convert::get_charset($text))
 			$this->charset = $this->file_charset = $encoding;
 
 		// convert document to UTF-8
@@ -659,7 +659,7 @@ class vcard_convert extends Contact_Vcard_Parse
 	 * @access private
 	 * @static
 	 */
-	function get_encoding($string)
+	function get_charset($string)
 	{
 		if (substr($string, 0, 4) == "\0\0\xFE\xFF") return 'UTF-32BE';  // Big Endian
 		if (substr($string, 0, 4) == "\xFF\xFE\0\0") return 'UTF-32LE';  // Little Endian
@@ -689,15 +689,16 @@ class vcard_convert extends Contact_Vcard_Parse
 	 */
 	function is_utf8($string)
 	{
-		return preg_match('%^(?:
-			[\xC2-\xDF][\x80-\xBF]         # non-overlong 2-byte
-			| \xE0[\xA0-\xBF][\x80-\xBF]   # excluding overlongs
-			| [\xE1-\xEC\xEE\xEF][\x80-\xBF]{2}  # straight 3-byte
-			| \xED[\x80-\x9F][\x80-\xBF]         # excluding surrogates
-			| \xF0[\x90-\xBF][\x80-\xBF]{2}      # planes 1-3
-			| [\xF1-\xF3][\x80-\xBF]{3}          # planes 4-15
-			| \xF4[\x80-\x8F][\x80-\xBF]{2}      # plane 16
-	   		)*$%xs', $string);
+		return preg_match('/\A(
+			[\x09\x0A\x0D\x20-\x7E]
+			| [\xC2-\xDF][\x80-\xBF]
+			| \xE0[\xA0-\xBF][\x80-\xBF]
+			| [\xE1-\xEC\xEE\xEF][\x80-\xBF]{2}
+			| \xED[\x80-\x9F][\x80-\xBF]
+			| \xF0[\x90-\xBF][\x80-\xBF]{2}
+			| [\xF1-\xF3][\x80-\xBF]{3}
+			| \xF4[\x80-\x8F][\x80-\xBF]{2}
+			)*\z/xs', substr($string, 0, 4096));
 	}
 	
 }  // end class vcard_convert
